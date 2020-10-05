ceb5e3e8ac17fdae40fe1b7c8fd74a5886256cd7
DeCoder <rao.sush004@gmail.com>
2020-10-05 16:04:55 +0530

    Update username

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   # The gmail address specified here will be used to send emails to recipients.
-  default from: '<gmail_address>'
+  default from: Rails.application.credentials.email[:username]
 end
